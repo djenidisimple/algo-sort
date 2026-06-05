5843c186af9fd7074002ade2c6b098bf2aa50e47
djenidisimple <djenidigauss@gmail.com>
2026-06-05 14:02:18 +0300

    new version optimal quick_sort

--- merge_sort.py
+++ merge_sort.py
@@ -18,4 +18,21 @@ def merge_sort(array):
     sort_left = merge_sort(left)
     sort_right = merge_sort(right)
     return merge(sort_left, sort_right)
-print(merge_sort([6, 5, 3, 1, 4, 2, 7]))
+# print(merge_sort([6, 5, 3, 1, 4, 2, 7]))
+
+def merge_div(left_value, right_value):
+    return left_value + right_value if left_value <= right_value else left_value - right_value
+
+
+def divide(array):
+     if len(array) == 0:
+        return 0
+     if len(array) == 2:
+        return array[0] + array[1] if array[0] <= array[1] else array[0] - array[1]
+     elif len(array) <= 1:
+        return array[0]
+     left , right = array[:len(array)//2], array[len(array)//2:]
+     left_divide = divide(left)
+     right_divide = divide(right)
+     return merge_div(left_divide, right_divide)
+print(divide([10, 1, 100, 10, 1000, 100, 1000, 1000, 1000]))
